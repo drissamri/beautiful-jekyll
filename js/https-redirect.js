33d0c04d2e5b0a7a178154f75b051c9900717d39
Driss Amri <driss.amri@telenet.be>
2015-05-18 10:42:46 +0200

    Turn of HTTPS

--- js/https-redirect.js
+++ js/https-redirect.js
@@ -1,4 +1,4 @@
   var host = "drissamri.be";
   if ((host === window.location.host) && (window.location.protocol !== "https:")) {
-    window.location.protocol = "https";
+    //window.location.protocol = "https";
   }
